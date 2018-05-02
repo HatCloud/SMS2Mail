95a0205cad1eaffbf56f00510cdd8a467d70ad05
Hat_Cloud <github@hatcloud.me>
2018-05-02 19:58:34 +0800

    Reafctor. modify some text from init and create necessary packages
    Signed-off-by: Hat_Cloud <github@hatcloud.me>

--- app/src/main/java/me/hatcloud/sms2mail/ui/MainActivity.kt
+++ app/src/main/java/me/hatcloud/sms2mail/ui/MainActivity.kt
@@ -1,24 +1,22 @@
-package me.hatcloud.sms2mail
+package me.hatcloud.sms2mail.ui
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import me.hatcloud.sms2mail.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_toggle -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_messages -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_configuration -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
---
+++ app/src/main/java/me/hatcloud/sms2mail/utils/KotlinExt.kt
@@ -0,0 +1,31 @@
+package me.hatcloud.sms2mail.utils
+
+import android.app.Activity
+import android.content.Context
+import android.support.annotation.IdRes
+import android.view.View
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Int.dp2px(context: Context?): Int {
+    val scale = context?.resources?.displayMetrics?.density?.toInt()
+    return (this * (scale ?: 2) + 0.5f).toInt()
+}
+
+fun Any?.toString(): String {
+    if (this == null) return "null"
+    return toString()
+}
+
+fun <T : View> Activity.bind(@IdRes idRes: Int): Lazy<T> {
+    return lazy { findViewById<T>(idRes) }
+}
+
+fun <T : View> View.bind(@IdRes idRes: Int): Lazy<T> {
+    return lazy { findViewById<T>(idRes) }
+}
+
+
