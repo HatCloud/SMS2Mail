cfb1749751fc05099d721c1547098ab6983ffb9e
Hat_Cloud <mr.hatcloud@gmail.com>
2018-05-02 18:43:39 +0800

    Chore. create project
    Signed-off-by: Hat_Cloud <github@hatcloud.me>

---
+++ app/src/androidTest/java/me/hatcloud/sms2mail/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package me.hatcloud.sms2mail
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.hatcloud.sms2mail", appContext.packageName)
+    }
+}
